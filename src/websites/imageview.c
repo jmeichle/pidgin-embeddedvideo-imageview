de21f5978fe8cd55c682276b014b6f5ef4099a5a
John Meichle <john.meichle@gmail.com>
2014-08-29 01:23:23 -0400

    Fresh import from pidgin-embeddedvideo-imageview.tar.gz http://code.google.com/p/pidgin-embeddedvideo/issues/detail?id=18

---
+++ src/websites/imageview.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  imageview.c
+ *
+ *    Description:  just for view the image file
+ *
+ *        Version:  1.0
+ *        Created:  2010年04月05日 15时02分58秒 CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lerosua (), lerosua@gmail.com
+ *        Company:  Cyclone
+ *
+ * =====================================================================================
+ */
+
+#include <websites.h>
+
+WebsiteInfo imageview= {
+    "imageview",
+    "^http://.*\\.(jpg|png|gif|jpeg|xpm|bmp)$",
+    NULL,
+    NULL
+};
+
